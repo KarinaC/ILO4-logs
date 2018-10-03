0a72048ed3dce61e651fcbfce26f4fa33f29a7c9
karina Ines costa <karmukis@tesla.karmukis>
2018-10-03 17:05:11 -0300

    Traying to make arguments work in wrapper

--- ILO-logs2.sh
+++ ILO-logs2.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/expect
 #!/bin/bash 
 
 #Usage <ip> <user> <pass>
 
+#DATE=date +"%m/%d/%y"
 
+
+/usr/bin/expect - <<EOF
 set timeout 60
 
 spawn ssh [lindex $argv 1]@[lindex $argv 0]
@@ -11,7 +13,7 @@ spawn ssh [lindex $argv 1]@[lindex $argv 0]
 expect "yes/no" {
         send "yes\r"
         expect "*?assword" { send "[lindex $argv 2]\r" }
-		        } "*?assword" { send "[lindex $argv 2]\r" }
+} "*?assword" { send "[lindex $argv 2]\r" }
 
 
 expect "*hpiLO-> " { send "show /system1/log1\r" } >> /tmp/ILO.log
@@ -25,4 +27,4 @@ expect "*hpiLO-> " { send "show /system1/log1\r" } >> /tmp/ILO.log
 #expect "# " { send "exit\r" }
 expect "*hpiLO-> " { send "exit\r" }
 # interact
-
+EOF
