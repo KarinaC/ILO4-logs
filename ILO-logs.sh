2423007df28b75d26ecb7e7c7fa5947fb4eb8340
karina Ines costa <karmukis@tesla.karmukis>
2018-10-03 16:01:04 -0300

    First version of ILO-logs script

---
+++ ILO-logs.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/expect
+
+#Usage <ip> <user> <pass>
+
+
+set timeout 60
+
+spawn ssh [lindex $argv 1]@[lindex $argv 0]
+
+expect "yes/no" {
+        send "yes\r"
+        expect "*?assword" { send "[lindex $argv 2]\r" }
+		        } "*?assword" { send "[lindex $argv 2]\r" }
+
+
+expect "*hpiLO-> " { send "show /system1/log1\r" } >> /tmp/ILO.log
+#expect "# " { send "scope log\r" }
+#expect "# " { send "show entries\r" }
+ expect {
+    -ex "--More--" { send "\r"; exp_continue }
+    "*hpiLO-> " { send "exit\r" }
+}
+#expect "# " { send "exit\r" }
+#expect "# " { send "exit\r" }
+expect "*hpiLO-> " { send "exit\r" }
+# interact
+
