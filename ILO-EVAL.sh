837aa64a56851f4bbe7ad67156f03e6db88389f2
karina Ines costa <karmukis@tesla.karmukis>
2018-10-04 14:42:20 -0300

    Clean the info search

--- ILO-EVAL.sh
+++ ILO-EVAL.sh
@@ -10,9 +10,9 @@ echo "" > /tmp/iloout.log
 #echo "Log del día $date" >> /tmp/ilotest.log 
 
 # Obtenemos los últimos 6 records logs y limpiamos la data
-for rec in `/home/karmukis/Documents/CLARO-ARGENTINA/SCRIPTS/EXPECT/ILO4-logs/ILO-logs.sh "$1" "$2" "$3" "$4" | grep record | tail -6 | awk -F 'cord' '{print $2}'` 
+for rec in `/home/karmukis/Documents/CLARO-ARGENTINA/SCRIPTS/EXPECT/ILO4-logs/ILO-logs.sh "${1}" "${2}" "${3}" "${4}" | grep record | tail -6 | awk -F 'cord' '{print $2}'` 
   do
-    /home/karmukis/Documents/CLARO-ARGENTINA/SCRIPTS/EXPECT/ILO4-logs/ILO-logs.sh "${1}" "${2}" "${3}" "show /system1/log1/record${rec}"  | awk '/Targets/,/Verbs/' >> /tmp/iloout.log
+    /home/karmukis/Documents/CLARO-ARGENTINA/SCRIPTS/EXPECT/ILO4-logs/ILO-logs.sh "${1}" "${2}" "${3}" "show /system1/log1/record${rec}"  | awk '/Properties/,/Verbs/' >> /tmp/iloout.log
   done
 
 
